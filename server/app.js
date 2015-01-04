593ea35af3b016c9f568a02bbc6a50cb070ddfc2
Carlo Cannas <carlo94@gmail.com>
2015-01-05 00:01:30 +0100

    Small additions to server/app.js
    I took the server script I was working on some time ago and commited it.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,26 @@
+"use strict";
+
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world');
+var mysql = require('db-mysql');
+
+
+app.use(bodyParser.json({strict: true}))
+
+app.post('/signup', function(req, res){
+	var user = req.body.username;
+	var mail = req.body.email;
+	var password = req.body.password;
+	var phone = req.body.phone;
+
+	res.send();
 });
 
-app.listen(3000);
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
